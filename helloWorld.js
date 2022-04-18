0a4467f91d03ffebde5eabc9010e7e6c1c44ce95
Munna-Mahmud <learnermunna@gmail.com>
2022-04-19 03:15:08 +0600

    days 3 done

---
+++ helloWorld.js
@@ -0,0 +1,13 @@
+// 1 days 
+function greeting(parameterVariable) {
+    // This line prints 'Hello, World!' to the console:
+    console.log('Hello, World!');
+    //  console.log('Welcome to 10 Days of JavaScript!');
+    console.log(parameterVariable)
+    // Write a( line of code that prints parameterVariable to stdout using console.log:
+
+}
+
+// 2nd days 
+
+
